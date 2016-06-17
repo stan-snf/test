cce9586ad976c3309c219d6bf86599aba470a95d
Stanley Ong <charlesong@creatinginfo.com>
2016-06-17 17:05:51 +0800

    updated index

---
+++ js/sw.js
@@ -0,0 +1,14 @@
+this.addEventListener('install', function(e) {
+	event.waitUntil(caches.create('site-v1').then(function(cache) {
+		return cache.add({
+			'/test/',
+			'/test/css/style.css'
+		});
+	}));
+});
+
+this.addEventListener('fetch', function(e) {
+	e.respondWith(
+		caches.match(e.request)
+	);
+});
