cb20ce63a24b20b5fb0fcc4eb3526f20376a1efe
Stanley Ong <charlesong@creatinginfo.com>
2016-06-17 18:14:53 +0800

    changes

--- sw.js
+++ sw.js
@@ -2,7 +2,13 @@ this.addEventListener('install', function(e) {
 	e.waitUntil(caches.open('site-v1').then(function(cache) {
 		return cache.addAll([
 			'/test/',
-			'/test/css/style.css'
+			'/test/index.html',
+			'/test/css/style.css',
+			'/test/images/logo.png',
+			'/test/images/family-large.jpg',
+			'/test/images/meeting.jpg',
+			'/test/images/handshake.jpg',
+			'/test/images/family-small.jpg',
 		]);
 	}));
 });
